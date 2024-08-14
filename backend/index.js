de11217752c2808d92dd60e05ed422939dd32f2b
zulkefal <zulkefal.khan705@gmail.com>
2024-08-14 13:01:18 +0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const connectDB= require('./config/ConnectDb');
 
 dotenv.config({});
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
