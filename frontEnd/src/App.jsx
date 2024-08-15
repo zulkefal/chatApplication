289090c19942a2e049f56f8fda9f9001681daa4f
zulkefal <zulkefal.khan705@gmail.com>
2024-08-15 15:06:19 +0500

    commit

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './component/HomePage'
+import Login from './component/Login'
+import SignUp from './component/SignUp'
 
 function App() {
 
   return (
     <>
-      
-    </>
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <Router>
+    
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+    </Router>
+  
+     </>
   )
-}
+} 
 
 export default App
