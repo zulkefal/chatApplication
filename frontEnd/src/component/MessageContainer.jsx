dbc3b701d497849f54c7b49b3871679ff819d970
zulkefal <zulkefal.khan705@gmail.com>
2024-08-16 15:47:57 +0500

    commit

--- frontEnd/src/component/MessageContainer.jsx
+++ frontEnd/src/component/MessageContainer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import SendInput from "./SendInput";
 import Messages from "./Messages";
+import { useSelector } from "react-redux";
 
 const MessageContainer = () => {
+  const {selectedUser} = useSelector(state=>state.user);
+
   return (
       <div className="w-full md:min-w-[450px] flex flex-col">
         <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2">
@@ -10,14 +13,14 @@ const MessageContainer = () => {
             <div className="w-12 rounded-full">
               {" "}
               <img
-                src="https://imgs.search.brave.com/ZlMA1xyb5O_WINlJ1KTJPjXirJamlkRY4vG4wWqequQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvY29vbC1wcm9m/aWxlLXBpY3R1cmUt/ODdoNDZnY29iamw1/ZTR4dS5qcGc"
+                src={selectedUser?.profilePhoto}
                 alt="user-profile"
               />
             </div>
           </div>
           <div className="">
             <div className="flex justify-between gap-2">
-              <p className="text-white">Zulkefal Khan</p>
+              <p className="text-white">{selectedUser?.fullName}</p>
             </div>
           </div>
         </div>
--- frontEnd/src/component/SendInput.jsx
+++ frontEnd/src/component/SendInput.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSend } from "react-icons/io5";
 
 const SendInput = () => {
+  const [message,setMessage] = useState('');
+
+  const onSubmitHandler = (e)=>{
+    e.preventDefault();
+    alert(message);
+    setMessage('');
+  }
   return (
-    <form className='px-4 my-3'>
+    <form onSubmit={onSubmitHandler} className='px-4 my-3'>
       <div className='w-full relative'>
-        <input type="text" placeholder='Send a Message..' className='border text-sm p-3 border-zinc-500 rounded-lg block w-full bg-gray-600 text-white'/>
-        <button className='absolute flex inset-y-0 end-0 items-center pr-4 hover:text-white'><IoSend/></button>
+        <input type="text" placeholder='Send a Message..' 
+        value={message}
+        onChange={(e)=>setMessage(e.target.value)}
+        className='border text-sm p-3 border-zinc-500 rounded-lg block w-full bg-gray-600 text-white'/>
+        <button type='submit' className='absolute flex inset-y-0 end-0 items-center pr-4 hover:text-white'><IoSend/></button>
       </div>
     </form>
   )
