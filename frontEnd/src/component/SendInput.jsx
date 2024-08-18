8f04b807cf0898792e2bd937b009f5f378300b1b
zulkefal <zulkefal.khan705@gmail.com>
2024-08-18 23:38:38 +0500

    commit

--- frontEnd/src/component/SendInput.jsx
+++ frontEnd/src/component/SendInput.jsx
@@ -29,9 +29,7 @@ const SendInput = () => {
         className='border text-sm p-3 border-zinc-500 rounded-lg block w-full bg-gray-600 text-white'/>
         <button type='submit' className='absolute flex inset-y-0 end-0 items-center pr-4 hover:text-white'><IoSend/></button>
       </div>
-      <div>
-        <h1>All Test will be here</h1>
-      </div>
+    
     </form>
   )
 }
